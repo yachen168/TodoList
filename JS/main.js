9fb4db42b36b93f15a63870cb68d292c56bc51ca
yachen <io260188@gmail.com>
2020-05-02 01:46:05 +0800

    feat: task item

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,13 @@
 // fake data
 let data = {
     lists: [{
-        id: 1,
         task: '吃飯',
         deadline: [],
         file: [],
         comment: '',
         isImportant: false,
-        completed: false
+        isCompleted: false
     }, {
-        id: 2,
         task: '睡覺',
         deadline: [],
         file: [],
@@ -22,6 +20,21 @@ let data = {
 let methods = {
     getNumTasksLeft: () => {
         return data.lists.length;
+    },
+    addNewTask: () => {
+        data.lists.push({
+            task: '哈哈',
+            deadline: [],
+            file: [],
+            comment: '',
+            isImportant: false,
+            isCompleted: false
+        })
+        return renderTasks();
+    },
+    updateOldTaskName: () => {
+        const inputsOldTaskNames = document.querySelectorAll('.old-task-name');
+        data.lists.forEach((item, i) => inputsOldTaskNames[i].value = item.task);
     }
 }
 
@@ -35,7 +48,7 @@ function renderTasks() {
         <div class="task-bar" data-id="${i+1}">
         <label>
             <input class="checkbox" type="checkbox">
-            <h2>${ item.task }</h2>
+            <input class="old-task-name" type="text" disabled>
         </label>
         <div class="icon-wrapper">
             <i class="fas fa-star"></i>
@@ -62,15 +75,15 @@ function renderTasks() {
                 </div>
             </div>
             <div class="card-footer">
-                <button class="cancel"><i class="fas fa-times"></i>Cancel</button>
-                <button class="addTask"><i class="fas fa-plus"></i>Save</button>
+                <button class="buttonCancel"><i class="fas fa-times"></i>Cancel</button>
+                <button class="buttonConfirm"><i class="fas fa-plus"></i>Save</button>
             </div>
         </div>
     </div>
         `;
     })
-
     taskItem.innerHTML = htmlString;
+    methods.updateOldTaskName();
 }
 
 // ======== render how many tasks left ========
@@ -98,17 +111,22 @@ navItems.forEach(navItem => {
 // ======== 點擊星星，標記重要待辦事項 =======
 const stars = document.querySelectorAll('.fa-star');
 const taskBars = document.querySelectorAll('.task-bar');
+const inputNewTaskName = document.querySelector('.new-task-name');
+const inputsOldTaskNames = document.querySelectorAll('.old-task-name');
 
 stars.forEach((star, i) => star.addEventListener('click', () => {
     toggleClass(taskBars[i], 'task-bar-important');
     toggleClass(star, 'star-active');
+    toggleClass(inputNewTaskName, 'task-name-important');
+    // minus star of new task
+    toggleClass(inputsOldTaskNames[i - 1], 'task-name-important');
 }))
 
 // ======== 點擊 +Add，出現新增事項編輯區  ========
-const addNewTask = document.querySelector('.add-new-task');
+const buttonAddNewTask = document.querySelector('.add-new-task');
 const editNewTaskArea = document.querySelector('.edit-new-task-area');
 
-addNewTask.addEventListener('click', () => {
+buttonAddNewTask.addEventListener('click', () => {
     toggleClass(editNewTaskArea, 'edit-area-active');
 });
 
@@ -120,15 +138,26 @@ pens.forEach((pen, i) => {
     pen.addEventListener('click', () => {
         toggleClass(oldTaskAreas[i], 'edit-area-active');
         toggleClass(pen, 'pen-active');
+        inputsOldTaskNames[i].disabled = false;
     })
 })
 
 // 點擊取消，關閉編輯區
-const cancelButtons = document.querySelectorAll('.cancel');
+const cancelButtons = document.querySelectorAll('.buttonCancel');
 
-cancelButtons.forEach(cancelButton => cancelButton.addEventListener('click', () => {
+cancelButtons.forEach((cancelButton, i) => cancelButton.addEventListener('click', () => {
     toggleClass(cancelButton.parentNode.parentNode.parentNode, 'edit-area-active');
+    toggleClass(pens[i - 1], 'pen-active');
 }))
+
+// 點擊確定，關閉編輯區，並添加 task 至畫面
+const confirmButtons = document.querySelectorAll('.buttonConfirm');
+
+confirmButtons.forEach((confirmButton, i) => confirmButton.addEventListener('click', () => {
+    toggleClass(confirmButton.parentNode.parentNode.parentNode, 'edit-area-active');
+    toggleClass(pens[i - 1], 'pen-active');
+}))
+
 
 
 // ======== toggle css class ========
