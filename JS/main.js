3a184b94d2825f6afeb5040fb7a2ef4faab5f114
yachen <io260188@gmail.com>
2020-05-02 14:41:33 +0800

    fix: buttons

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,6 @@
 // fake data
 let data = {
+    NumTasksLeft: 0,
     lists: [{
         task: '吃飯',
         deadline: [],
@@ -19,17 +20,19 @@ let data = {
 
 let methods = {
     getNumTasksLeft: () => {
-        return data.lists.length;
+        data.NumTasksLeft = data.lists.length;
+        return data.NumTasksLeft;
     },
     addNewTask: () => {
         data.lists.push({
-            task: '哈哈',
+            task: '看電視',
             deadline: [],
             file: [],
             comment: '',
             isImportant: false,
             isCompleted: false
         })
+        renderNumsTasksLeft();
         return renderTasks();
     },
     updateOldTaskName: () => {
@@ -45,13 +48,13 @@ function renderTasks() {
     let htmlString = '';
     data.lists.forEach((item, i) => {
         htmlString = htmlString + `
-        <div class="task-bar" data-id="${i+1}">
+        <div class="old-task-bar" data-id="${i+1}">
         <label>
             <input class="checkbox" type="checkbox">
             <input class="old-task-name" type="text" disabled>
         </label>
         <div class="icon-wrapper">
-            <i class="fas fa-star"></i>
+            <i class="fas fa-star old-task-star"></i>
             <i class="fas fa-pen"></i>
         </div>
     </div>
@@ -67,7 +70,10 @@ function renderTasks() {
                 </div>
                 <div class="file">
                     <h3><i class="far fa-file"></i>File</h3>
-                    <button class="plus">+</button>
+                    <label class="upload_cover">
+                        <input class="upload_input" type="file">
+                        <span class="upload_icon">+</span>
+                    </label>
                 </div>
                 <div class="comment">
                     <h3><i class="far fa-comment-dots"></i>Comment</h3>
@@ -75,8 +81,8 @@ function renderTasks() {
                 </div>
             </div>
             <div class="card-footer">
-                <button class="buttonCancel"><i class="fas fa-times"></i>Cancel</button>
-                <button class="buttonConfirm"><i class="fas fa-plus"></i>Save</button>
+                <button class="buttonCancelEdit"><i class="fas fa-times"></i>Cancel</button>
+                <button class="buttonConfirmEdit"><i class="fas fa-plus"></i>Save</button>
             </div>
         </div>
     </div>
@@ -84,9 +90,55 @@ function renderTasks() {
     })
     taskItem.innerHTML = htmlString;
     methods.updateOldTaskName();
+
+    // ======== 點擊鉛筆，出現該事項編輯區 ========
+    const oldTaskAreas = document.querySelectorAll('.edit-old-task-area');
+    const pens = document.querySelectorAll('.task-item .fa-pen');
+    pens.forEach((pen, i) => {
+        pen.addEventListener('click', () => {
+            toggleClass(oldTaskAreas[i], 'edit-area-active');
+            toggleClass(pen, 'pen-active');
+            inputsOldTaskNames[i].disabled = false;
+        })
+    })
+
+    // 點擊取消，關閉 old task 編輯區
+    const cancelEditButtons = document.querySelectorAll('.buttonCancelEdit');
+
+    cancelEditButtons.forEach((cancelEditButton, i) => cancelEditButton.addEventListener('click', () => {
+        toggleClass(cancelEditButton.parentNode.parentNode.parentNode, 'edit-area-active');
+        toggleClass(pens[i], 'pen-active');
+    }))
+
+    // 點擊「確定編輯」，更新原有 task
+    const confirmEditButtons = document.querySelectorAll('.buttonConfirmEdit');
+    confirmEditButtons.forEach((confirmEditButton, i) => {
+        confirmEditButton.addEventListener('click', (e) => {
+            toggleClass(confirmEditButtons[i].parentNode.parentNode.parentNode, 'edit-area-active');
+            toggleClass(pens[i], 'pen-active');
+        })
+    })
+
+    // ======== 點擊 old task 星星，標記重要待辦事項 =======
+    const oldTaskStars = document.querySelectorAll('.old-task-star');
+    const oldTaskBars = document.querySelectorAll('.old-task-bar');
+    const inputsOldTaskNames = document.querySelectorAll('.old-task-name');
+    oldTaskStars.forEach((oldTaskStar, i) => {
+        if (data.lists[i].isImportant) {
+            oldTaskBars[i].classList.add('task-bar-important');
+            inputsOldTaskNames[i].classList.add('task-name-important');
+            oldTaskStars[i].classList.add('star-active');
+        }
+        oldTaskStar.addEventListener('click', () => {
+            data.lists[i].isImportant = !data.lists[i].isImportant;
+            toggleClass(oldTaskBars[i], 'task-bar-important');
+            toggleClass(oldTaskStar, 'star-active');
+            toggleClass(inputsOldTaskNames[i], 'task-name-important');
+        })
+    })
 }
 
-// ======== render how many tasks left ========
+// ======== render 剩餘 task ========
 const numsTasksLeft = document.querySelector('.numbers-tasks-left');
 
 function renderNumsTasksLeft() {
@@ -108,20 +160,6 @@ navItems.forEach(navItem => {
     })
 })
 
-// ======== 點擊星星，標記重要待辦事項 =======
-const stars = document.querySelectorAll('.fa-star');
-const taskBars = document.querySelectorAll('.task-bar');
-const inputNewTaskName = document.querySelector('.new-task-name');
-const inputsOldTaskNames = document.querySelectorAll('.old-task-name');
-
-stars.forEach((star, i) => star.addEventListener('click', () => {
-    toggleClass(taskBars[i], 'task-bar-important');
-    toggleClass(star, 'star-active');
-    toggleClass(inputNewTaskName, 'task-name-important');
-    // minus star of new task
-    toggleClass(inputsOldTaskNames[i - 1], 'task-name-important');
-}))
-
 // ======== 點擊 +Add，出現新增事項編輯區  ========
 const buttonAddNewTask = document.querySelector('.add-new-task');
 const editNewTaskArea = document.querySelector('.edit-new-task-area');
@@ -130,35 +168,43 @@ buttonAddNewTask.addEventListener('click', () => {
     toggleClass(editNewTaskArea, 'edit-area-active');
 });
 
-// ======== 點擊鉛筆，出現該事項編輯區 ========
+// 點擊確定「新增」，關閉編輯區，並添加 task 至畫面
+const confirmAddButton = document.querySelector('.buttonConfirmAdd');
 const pens = document.querySelectorAll('.task-item .fa-pen');
-const oldTaskAreas = document.querySelectorAll('.edit-old-task-area');
 
-pens.forEach((pen, i) => {
-    pen.addEventListener('click', () => {
-        toggleClass(oldTaskAreas[i], 'edit-area-active');
-        toggleClass(pen, 'pen-active');
-        inputsOldTaskNames[i].disabled = false;
-    })
+confirmAddButton.addEventListener('click', () => {
+    toggleClass(confirmAddButton.parentNode.parentNode.parentNode, 'edit-area-active');
+    toggleClass(pens[0], 'pen-active');
+    methods.addNewTask();
 })
 
-// 點擊取消，關閉編輯區
-const cancelButtons = document.querySelectorAll('.buttonCancel');
 
-cancelButtons.forEach((cancelButton, i) => cancelButton.addEventListener('click', () => {
-    toggleClass(cancelButton.parentNode.parentNode.parentNode, 'edit-area-active');
-    toggleClass(pens[i - 1], 'pen-active');
+// 點擊取消，關閉 new task 編輯區
+const cancelAddButton = document.querySelector('.buttonCancelAdd');
+
+cancelAddButton.addEventListener('click', () => {
+    toggleClass(cancelAddButton.parentNode.parentNode.parentNode, 'edit-area-active');
+})
+
+
+// checkbox (已完成)，更改 isCompleted 狀態為 true
+const checkboxes = document.querySelectorAll('.task-item .checkbox');
+
+checkboxes.forEach((checkbox, i) => checkbox.addEventListener('click', () => {
+    data.lists[i].isCompleted = !data.lists[i].isCompleted;
+    console.log(data.lists[i].isCompleted);
 }))
 
-// 點擊確定，關閉編輯區，並添加 task 至畫面
-const confirmButtons = document.querySelectorAll('.buttonConfirm');
 
-confirmButtons.forEach((confirmButton, i) => confirmButton.addEventListener('click', () => {
-    toggleClass(confirmButton.parentNode.parentNode.parentNode, 'edit-area-active');
-    toggleClass(pens[i - 1], 'pen-active');
-}))
+// ======== 點擊 new task 星星，標記重要待辦事項 =======
+const newTaskStar = document.querySelector('.new-task-star');
+const mewTaskBar = document.querySelector('.new-task-bar');
+const inputNewTaskName = document.querySelector('.new-task-name');
 
-
+newTaskStar.addEventListener('click', () => {
+    toggleClass(mewTaskBar, 'task-bar-important');
+    toggleClass(inputNewTaskName, 'task-name-important');
+})
 
 // ======== toggle css class ========
 function toggleClass(nodeItem, cssClass) {
