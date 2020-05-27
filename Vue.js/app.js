7b2c8fe342b683ac028c9877032b36e7cf5f4997
yachen <io260188@gmail.com>
2020-05-27 13:40:04 +0800

    docs: rename

--- Vue.js/app.js
+++ Vue.js/app.js
@@ -2,31 +2,7 @@
     const app = new Vue({
         el: '#app',
         data: {
-            todos: [{
-                todoTitle: '吃飯',
-                todoComment: '肯德基',
-                todoDate: '',
-                todoTime: '',
-                isStared: false,
-                isCompleted: false,
-                isEditing: false
-            }, {
-                todoTitle: '睡覺',
-                todoComment: '三小時',
-                todoDate: '2015-12-12',
-                todoTime: '',
-                isStared: false,
-                isCompleted: false,
-                isEditing: false
-            }, {
-                todoTitle: '看電視',
-                todoComment: '一小時',
-                todoDate: '',
-                todoTime: '',
-                isStared: false,
-                isCompleted: false,
-                isEditing: false
-            }],
+            todos: JSON.parse(localStorage.getItem('todos')) || [],
             newTodo: {
                 todoTitle: '',
                 todoComment: '',
@@ -34,6 +10,7 @@
                 todoTime: '',
                 isStared: false,
                 isCompleted: false,
+                isEditing: false
             },
             navItemState: 'all',
             isEditingNewTodo: false
@@ -46,9 +23,11 @@
                     todoDate: this.newTodo.todoDate,
                     todoTime: this.newTodo.todoTime,
                     isStared: this.newTodo.isStared,
-                    isCompleted: this.newTodo.isCompleted
+                    isCompleted: this.newTodo.isCompleted,
+                    isEditing: false
                 });
                 this.clearNewTodo();
+                this.setLocalStorage();
                 console.log(this.todos);
             },
             clearNewTodo() {
@@ -59,21 +38,44 @@
                 this.newTodo.isStared = false;
                 this.newTodo.isCompleted = false;
             },
+            editDone(todo, index) {
+                this.todos[index] = todo;
+                this.toggleEditCard(index);
+                this.setLocalStorage();
+            },
             deleteTodo(index) {
                 this.todos.splice(index, 1);
+                this.setLocalStorage();
             },
+            setLocalStorage() {
+                localStorage.setItem('todos', JSON.stringify(this.todos));
+            },
+            // getLocalStorage() {
+            //     return JSON.parse(localStorage.getItem('todos')) || [];
+            // },
             markNewTodo() {
                 this.newTodo.isStared = !this.newTodo.isStared;
             },
             markTodo(index) {
                 this.todos[index].isStared = !this.todos[index].isStared;
+                this.setLocalStorage();
             },
             leftCounter() {
                 return this.todos.filter(todo => !todo.isCompleted).length;
             },
             completedCounter() {
                 return this.todos.filter(todo => todo.isCompleted).length;
-            }
+            },
+            toggleCompleted(index) {
+                this.todos[index].isCompleted = !this.todos[index].isCompleted;
+            },
+            toggleNewEditCard() {
+                this.isEditingNewTodo = !this.isEditingNewTodo;
+            },
+            toggleEditCard(index) {
+                this.todos[index].isEditing = !this.todos[index].isEditing;
+                console.log(this.todos);
+            },
         }
     })
 })()
