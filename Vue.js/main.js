7bb0c6fae964d2f0ca6d7e538d3ba63d73d6b04d
yachen <io260188@gmail.com>
2020-05-29 00:38:39 +0800

    feat: initial vue version

---
+++ Vue.js/main.js
@@ -0,0 +1,91 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        todos: JSON.parse(localStorage.getItem('todos')) || [],
+        newTodo: {
+            todoTitle: '',
+            todoComment: '',
+            todoDate: '',
+            todoTime: '',
+            isStared: false,
+            isCompleted: false,
+        },
+        state: 'all',
+        isEditing: false,
+        isEditingNewTodo: false
+    },
+    methods: {
+        addNewTodo() {
+            this.todos.push({
+                id: Date.now(),
+                todoTitle: this.newTodo.todoTitle,
+                todoComment: this.newTodo.todoComment,
+                todoDate: this.newTodo.todoDate,
+                todoTime: this.newTodo.todoTime,
+                isStared: this.newTodo.isStared,
+                isCompleted: this.newTodo.isCompleted,
+            });
+            this.clearNewTodo();
+            this.setLocalStorage();
+        },
+        clearNewTodo() {
+            this.newTodo.todoTitle = '';
+            this.newTodo.todoComment = '';
+            this.newTodo.todoDate = '';
+            this.newTodo.todoTime = '';
+            this.newTodo.isStared = false;
+            this.newTodo.isCompleted = false;
+        },
+        editDone(todo, index) {
+            this.todos[index] = todo;
+            this.toggleEditCard(todo);
+            this.setLocalStorage();
+        },
+        deleteTodo(index) {
+            this.todos.splice(index, 1);
+            this.setLocalStorage();
+        },
+        setLocalStorage() {
+            localStorage.setItem('todos', JSON.stringify(this.sortTodos));
+        },
+        markNewTodo() {
+            this.newTodo.isStared = !this.newTodo.isStared;
+        },
+        markTodo(todo) {
+            todo.isStared = !todo.isStared;
+            this.setLocalStorage();
+        },
+        toggleNewEditCard() {
+            this.isEditingNewTodo = !this.isEditingNewTodo;
+        },
+        toggleEditCard(todo) {
+            todo.isEditing = !todo.isEditing;
+        },
+        toggleCompleted(todo) {
+            todo.isCompleted = !todo.isCompleted;
+            this.setLocalStorage();
+        },
+        todoFilter() {
+            return this.todos.filter(todo => {
+                if (this.state === 'all') return this.todos;
+                if (this.state === 'inProgress') return !todo.isCompleted;
+                if (this.state === 'completed') return todo.isCompleted;
+            })
+        }
+    },
+    computed: {
+        sortTodos() {
+            return this.todoFilter().sort((a, b) => {
+                let scoreA = (a.isStared ? 100 : 0) + (a.isCompleted ? -200 : 0);
+                let scoreB = (b.isStared ? 100 : 0) + (b.isCompleted ? -200 : 0);
+                return scoreA - scoreB;
+            })
+        },
+        leftCounter() {
+            return this.todos.filter(todo => !todo.isCompleted).length;
+        },
+        completedCounter() {
+            return this.todos.filter(todo => todo.isCompleted).length;
+        }
+    }
+})
